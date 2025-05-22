b7d45ec8cedd8ac21716c9eadbe813fefdb96bbd
daferali20 <31751340+daferali20@users.noreply.github.com>
2025-05-22 15:57:28 +0300

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [recommendation, setRecommendation] = useState("");
+
+  const getRecommendation = async () => {
+    try {
+     const res = await axios.post("https://stock-recommendation-server.onrender.com/recommend", { symbol: "AAPL" });
+      setRecommendation(res.data.recommendation);
+    } catch (error) {
+      setRecommendation("حدث خطأ.");
+    }
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>📊 توصية لحظية</h2>
+      <button onClick={getRecommendation}>جلب التوصية</button>
+      <p>{recommendation}</p>
+    </div>
+  );
+}
+
+export default App;
